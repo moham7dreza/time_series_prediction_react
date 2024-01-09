620dc7bff795981f97bcc19064b1aa686ad413b4
Mohamadreza Rezaei <57709260+moham7dreza@users.noreply.github.com>
2024-01-09 22:54:38 +0330

    fix spinner css

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -5,9 +5,9 @@ export const Spinner = () => {
     return (
         <>
             <div className="App">
-                <header className="App-header">
+                <header className="App-header bg-white dark:bg-slate-900 dark:text-gray-400">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className={'text-2xl text-gray-400 font-bold capitalize mt-10'}>Data is begin to fetching from
+                    <h1 className={'text-2xl font-bold capitalize mt-10'}>Data is begin to fetching from
                         API application ...</h1>
                 </header>
             </div>
